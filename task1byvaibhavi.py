51da157c6289280a6333dde18b7800cd67d665ec
vaibhavi26112005 <v9612466@gmail.com>
2025-06-29 21:30:42 +0530

    Add files via upload

---
+++ task1byvaibhavi.py
@@ -0,0 +1,6 @@
+a=int(input('Enter first number: '))
+b=int(input('Enter second number: '))
+print('addition =',a+b)
+print('subtraction =',a-b)
+print('multiplication =',a*b)
+print('division =',a/b)
---
+++ task2byvaibhavi.py
@@ -0,0 +1,4 @@
+a=str(input('Enter your first name:'))
+b=str(input('Enter your second name:'))
+c=a+b
+print("hello,",c,"! Welcome to python program")
